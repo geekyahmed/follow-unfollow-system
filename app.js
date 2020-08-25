5908bb48ac3674b77bf3ea4806d9b8fa886b0d3e
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-25 23:43:25 +0100

    configured mongodb database

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const exphbs = require('express-handlebars').create({ defaultLayout: main })
+const exphbs = require('express-handlebars')
 const port = process.env.PORT || 1333
 const app = express()
 
 app.use(express())
 
-app.engine('handlebars', exphbs)
-app.set('view engine', exphbs)
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'default'
+  })
+)
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`)
---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const db = {
+  uri: 'mongodb:localhost:2707/follow-system'
+}
+
+const opts = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  keepAlive: 1
+}
+
+const connectDB = () => {
+  mongoose
+    .connect(db.uri, opts)
+    .then(() => {
+      console.log('MongoDB Connection Successful.')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+module.exports = connectDB
