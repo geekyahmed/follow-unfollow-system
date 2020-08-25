42ee8926e5095ce39c726039361f545b1bd67ab6
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-25 23:59:48 +0100

    create user model and fixed bugs

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
+const path = require('path')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
 const port = process.env.PORT || 1333
 const connectDB = require('./config/db')
 const app = express()
 
+//Setting Up Express
 app.use(express())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
+//Connect to MongoDB Database
 connectDB()
 
+//Configure Handlebars
 app.engine(
   'handlebars',
   exphbs({
@@ -15,6 +22,23 @@ app.engine(
   })
 )
 app.set('view engine', 'handlebars')
+
+//Setting up Passport
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+)
+
+//Allow Access For All Origins
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`)
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  followers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }
+  ],
+  following: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'user'
+    }
+  ],
+  password: {
+    type: String,
+    min: 4,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+const User = mongoose.model('user', UserSchema)
+module.exports = User
